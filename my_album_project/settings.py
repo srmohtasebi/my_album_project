27f09ada2ea92e039d253b8bbf4948f028f87430
srmohtasebi <s.r.mohtasebi@gmail.com>
2024-11-04 18:07:13 +0330

    Add STATIC_ROOT for static files

--- my_album_project/settings.py
+++ my_album_project/settings.py
@@ -121,13 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]  # اضافه کردن مسیر static
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 # CORS settings
